a9a94d3e47ba042ce38b0aec1946d8f660d5654b
S. M. Rafi <smrafialam@gmail.com>
2024-09-11 03:59:04 +0600

    database connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+const dotenv = require("dotenv");
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
+
+  mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected successfully');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
